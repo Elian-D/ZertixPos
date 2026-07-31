0067a3eaefbcd0165c57067f4afe3511bda978af
Elian-D <eliandavidbertrecuello19@gmail.com>
2026-07-31 10:18:24 -0400

    feat: registrar autoría explícita de apertura/cierre y motivo de descuadre en sesiones POS

--- app/Filters/Sales/Pos/SessionFilters/PosSessionFilters.php
+++ app/Filters/Sales/Pos/SessionFilters/PosSessionFilters.php
@@ -9,10 +9,13 @@ class PosSessionFilters extends QueryFilter
     protected function filters(): array
     {
         return [
-            'terminal_id' => PosSessionTerminalFilter::class,
-            'user_id'     => PosSessionUserFilter::class,
-            'status'      => PosSessionStatusFilter::class,
-            'from_date'   => PosSessionDateFilter::class,
+            'terminal_id'       => PosSessionTerminalFilter::class,
+            'user_id'           => PosSessionUserFilter::class,
+            'opened_by_user_id' => PosSessionOpenedByFilter::class,
+            'closed_by_user_id' => PosSessionClosedByFilter::class,
+            'status'            => PosSessionStatusFilter::class,
+            'difference_reason' => PosSessionDifferenceReasonFilter::class,
+            'from_date'         => PosSessionDateFilter::class,
         ];
     }
 }
