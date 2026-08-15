729ff656c05e46fcd86a0459780f1a71d64b4002
Elian-D <eliandavidbertrecuello19@gmail.com>
2026-08-15 15:38:14 -0400

    ui(layout): aplicar x-cloak en componentes de sidebar y tablas para evitar FOUC

--- resources/views/components/sidebar/title.blade.php
+++ resources/views/components/sidebar/title.blade.php
@@ -1,5 +1,6 @@
 <h3 class="mt-4 mb-1 px-2 text-xs font-semibold uppercase tracking-wider text-gray-400"
     x-show="isSidebarOpen || hasHover"
+    x-cloak
     x-transition:enter="transition ease-out duration-200"
     x-transition:enter-start="opacity-0 translate-x-2"
     x-transition:enter-end="opacity-100 translate-x-0">
